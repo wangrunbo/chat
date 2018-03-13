133ce1536c38ee8cfd88548304481ada63bb4f7c
wangrunbo <wangrunbo921@gmail.com>
2018-03-14 01:30:44 +0900

    up

--- run.py
+++ run.py
@@ -5,21 +5,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = b"Z'Nf91\x07wL\xf5\x12\x87~:#\x8aLpZW95\xdc\x1d"
 socketio = SocketIO(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @socketio.on('send message', namespace='/chat')
 def chat(message):
     emit('show message', {'data': message['data']}, broadcast=True)
 
+
 @socketio.on('connect', namespace='/chat')
 def connect():
-    emit('show message', {'data': 'Connected'})
+    emit('show message', {'data': '成功连接'})
+
 
 @socketio.on('disconnect', namespace='/chat')
 def disconnect():
     print('Client disconnected')
 
+
 if __name__ == '__main__':
-    socketio.run(app, port=4500, debug=True)
+    socketio.run(app, port=4500, host='0.0.0.0', debug=True)
